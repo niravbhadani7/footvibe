17bce082a0473bbf8640a4802287d635466bfee8
nirav <niravbhadani7@gmail.com>
2024-08-21 16:42:31 +0530

    signup complite

--- src/components/form/signup/index.jsx
+++ src/components/form/signup/index.jsx
@@ -7,21 +7,20 @@ export default function Signup() {
     username: "",
     email: "",
     password: "",
-  })
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  });
+
+  const handleChange = (el) => {
+    const { name, value } = el.target;
     setUserInfo({ ...userInfo, [name]: value });
     setErrors({ ...errors, [name]: "" });
-  }
+  };
 
-  const [errors, setErrors] = useState({})
-  console.log(errors)
+  const [errors, setErrors] = useState({});
   const validForm = () => {
     let error = {};
     let isValid = true;
 
-    if (userInfo.username === "") { 
+    if (userInfo.username === "") {
       error.username = "Username is required";
       isValid = false;
     }
@@ -29,36 +28,49 @@ export default function Signup() {
     if (userInfo.email === "") {
       error.email = "Email is required";
       isValid = false;
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(userInfo.email)){
+      error.email = "Invalid email address";
+      isValid = false;
     }
 
     if (userInfo.password === "") {
       error.password = "Password is required";
       isValid = false;
-    } 
+    }else if(userInfo.password.length < 6){
+      error.password = "Password must be at least 6 characters";
+      isValid = false;
+    }
     setErrors(error);
     return isValid;
-  }
-  const handleSubmit = (e) => {
+  };
+  const handleSubmit = () => {
     if (validForm()) {
-      
+      const userData = JSON.parse(localStorage.getItem("user")) || [];
+      userData.push(userInfo);
+      localStorage.setItem("user", JSON.stringify(userData));
+      setUserInfo({
+        username: "",
+        email: "",
+        password: "",
+      });
     }
-  }
+  };
   return (
     <div className="signup">
       <div className="container">
         <section>
           <h1>Sign up</h1>
           <label>
-            <span>Username  {userInfo.errors}</span>
-            <input type="text" name="username" onChange={handleChange}/>
+            Username <span>{errors.username}</span>
+            <input type="text" name="username" onChange={handleChange} value={userInfo.username}/>
           </label>
           <label>
-            <span>Email {userInfo.errors}</span>
-            <input type="email" name="email" onChange={handleChange}/>
+            Email <span>{errors.email}</span>
+            <input type="email" name="email" onChange={handleChange} value={userInfo.email}/>
           </label>
           <label>
-            <span>Password {userInfo.errors}</span>
-            <input type="password" name="password" onChange={handleChange}/>
+            Password <span>{errors.password}</span>
+            <input type="password" name="password" onChange={handleChange} value={userInfo.password}/>
           </label>
           <button onClick={handleSubmit}>Sign up</button>
           <p>
