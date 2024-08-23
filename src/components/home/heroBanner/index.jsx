5ad3d25507dc451b53174ad40433be08a86dade0
nirav <niravbhadani7@gmail.com>
2024-08-23 17:29:48 +0530

    initial commit

--- src/components/home/heroBanner/index.jsx
+++ src/components/home/heroBanner/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './herobanner.scss'
+import HeroBanner from '../../../assets/image/herobanner.jpeg'
 
 export default function Herobanner() {
   return (
     <div className='hero-banner'>
-      <h1>Step Into Comfort And Style With Every Stride.</h1>
-      <button>Shop Now</button>
+      <img src={HeroBanner}/>
       <div className='layer'></div>
     </div>
   )
